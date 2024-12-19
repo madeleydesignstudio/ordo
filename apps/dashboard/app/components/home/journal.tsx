714d35f65428fec2bb43ca1102e86135e7169acd
Dan <dev@madeleydesignstudio.org>
2024-12-19 21:55:43 +0000

    changed styles

--- apps/dashboard/app/components/home/journal.tsx
+++ apps/dashboard/app/components/home/journal.tsx
@@ -8,9 +8,9 @@ const Journal = () => {
       <div className="border border-dashed border-[#6B9CA9] h-full w-full rounded-lg p-1">
         <div className="border border-[#6B9CA9] rounded-lg w-full h-full bg-[F8FEFA]">
           <div className="p-2 font-bold text-[#6B9CA9]">Journal</div>
-          <div>
+          {/* <div>
             <TailwindEditor />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
