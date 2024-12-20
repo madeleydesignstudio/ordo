5d45a2be56cc2669e6b86c081804ce58913240ba
Dan <dev@madeleydesignstudio.org>
2024-12-20 09:11:56 +0000

    added tiptap

--- apps/dashboard/app/components/home/tailwind-editor.tsx
+++ apps/dashboard/app/components/home/tailwind-editor.tsx
@@ -1,19 +1,32 @@
+import "../../tiptap.css";
+
 import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
 import Text from "@tiptap/extension-text";
+import Placeholder from "@tiptap/extension-placeholder";
 
 import { EditorContent, useEditor } from "@tiptap/react";
 import React from "react";
 
 export default function TailwindEditor() {
   const editor = useEditor({
-    extensions: [Document, Paragraph, Text],
-    content: `
-      <p>
-        Start writing...
-      </p>
-      
-    `,
+    extensions: [
+      Document,
+      Paragraph,
+      Text,
+      Placeholder.configure({
+        // Use a placeholder:
+        placeholder: "Start writing...",
+        // Use different placeholders depending on the node type:
+        // placeholder: ({ node }) => {
+        //   if (node.type.name === 'heading') {
+        //     return 'What’s the title?'
+        //   }
+
+        //   return 'Can you add some further context?'
+        // },
+      }),
+    ],
   });
 
   return (
