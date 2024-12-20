b0a223a26c2948522fd8dcdf0124e9d095a5d7fc
Dan <dev@madeleydesignstudio.org>
2024-12-20 09:03:51 +0000

    added tiptap

--- apps/dashboard/app/components/home/tailwind-editor.tsx
+++ apps/dashboard/app/components/home/tailwind-editor.tsx
@@ -1,21 +1,26 @@
-"use client";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import Placeholder from "@tiptap/extension-placeholder";
+import { EditorContent, useEditor } from "@tiptap/react";
+import React from "react";
 
-import { EditorContent, EditorRoot, JSONContent } from "novel";
-import { useState } from "react";
-
-const TailwindEditor = () => {
-  const [content, setContent] = useState<JSONContent>();
+export default function TailwindEditor() {
+  const editor = useEditor({
+    extensions: [
+      Document,
+      Paragraph,
+      Text,
+      Placeholder.configure({
+        placeholder: "Write something …",
+      }),
+    ],
+  });
 
   return (
-    <EditorRoot>
-      <EditorContent
-        initialContent={content}
-        onUpdate={({ editor }) => {
-          const json = editor.getJSON();
-          setContent(json);
-        }}
-      />
-    </EditorRoot>
+    <EditorContent
+      className="text-xs font-light text-[#6B9CA9] h-full overflow-y-auto px-2"
+      editor={editor}
+    />
   );
-};
-export default TailwindEditor;
+}
