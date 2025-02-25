5adb019d9da31b6f0a82e4af6e48c0ec749e84e5
danielcmadeley <daniel@madeleydesignstudio.org>
2025-02-25 08:56:41 +0000

    added tanstack

--- apps/dashboard/app/routes/__root.tsx
+++ apps/dashboard/app/routes/__root.tsx
@@ -7,7 +7,9 @@ import {
   Scripts,
 } from "@tanstack/react-router";
 
-import appCss from "@/styles/app.css?url";
+import "@workspace/ui/globals.css";
+
+// import appCss from "@/styles/app.css?url";
 
 export const Route = createRootRoute({
   head: () => ({
@@ -23,12 +25,12 @@ export const Route = createRootRoute({
         title: "TanStack Start Starter",
       },
     ],
-    links: [
-      {
-        rel: "stylesheet",
-        href: appCss,
-      },
-    ],
+    // links: [
+    //   {
+    //     rel: "stylesheet",
+    //     href: appCss,
+    //   },
+    // ],
   }),
   component: RootComponent,
 });
--- apps/dashboard/app/routes/index.tsx
+++ apps/dashboard/app/routes/index.tsx
@@ -11,7 +11,9 @@ function Home() {
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <Button size="sm" variant="outline">
+          Button
+        </Button>
       </div>
     </div>
   );
