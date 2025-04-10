39b9bbe6f5b9de96b43a0eb03ec37e33e6f1b094
danielcmadeley <daniel@madeleydesignstudio.org>
2025-04-10 20:42:50 +0100

    test netlify

--- apps/dashboard/app/lib/auth-client.ts
+++ apps/dashboard/app/lib/auth-client.ts
@@ -1,5 +1,4 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  /** the base url of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.BETTER_AUTH_URL,
 });
--- apps/dashboard/app/lib/auth.ts
+++ apps/dashboard/app/lib/auth.ts
@@ -3,10 +3,20 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db/db"; // your drizzle instance
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
   }),
-  emailAndPassword: {
-    enabled: true,
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // 5 minutes
+    },
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
   },
 });
