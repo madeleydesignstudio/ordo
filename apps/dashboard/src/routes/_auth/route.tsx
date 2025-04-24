319a2dbbd248fc5358c2c7507286d18bd8432115
danielcmadeley <daniel@madeleydesignstudio.org>
2025-04-25 01:24:48 +0300

    added ccs

--- apps/dashboard/src/routes/_auth/route.tsx
+++ apps/dashboard/src/routes/_auth/route.tsx
@@ -17,7 +17,7 @@ export const Route = createFileRoute("/_auth")({
 
 function RouteComponent() {
   return (
-    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className=" flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
         <Outlet />
       </div>
