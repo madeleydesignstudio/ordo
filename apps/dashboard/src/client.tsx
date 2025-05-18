c2dd77d5441de5f9d77fc590383ad5c035de6d11
danielcmadeley <daniel@madeleydesignstudio.org>
2025-05-18 21:57:29 +0100

    added posthog

--- apps/dashboard/src/client.tsx
+++ apps/dashboard/src/client.tsx
@@ -3,12 +3,21 @@ import { StartClient } from "@tanstack/react-start";
 import { StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { createRouter } from "./router.js";
+import { PostHogProvider } from "posthog-js/react";
 
 const router = createRouter();
 
 hydrateRoot(
   document,
   <StrictMode>
-    <StartClient router={router} />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        debug: import.meta.env.MODE === "development",
+      }}
+    >
+      <StartClient router={router} />
+    </PostHogProvider>
   </StrictMode>,
 );
