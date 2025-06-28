e7489de844dc2e4da027cb314ca971a6ed3dfe98
danielcmadeley <daniel@madeleydesignstudio.org>
2025-06-28 20:00:17 +0100

    added ordo

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@ordo/ui"],
+}
+
+export default nextConfig
---
+++ packages/ui/eslint.config.js
@@ -0,0 +1,4 @@
+import { config } from "@ordo/eslint-config/react-internal"
+
+/** @type {import("eslint").Linter.Config} */
+export default config
