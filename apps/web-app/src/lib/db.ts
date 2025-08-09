9da94b4affd800f2c5148dadf78de94a57b63428
danielcmadeley <daniel@madeleydesignstudio.org>
2025-08-09 10:13:25 +0100

    added simplified stack

--- apps/web-app/src/lib/db.ts
+++
@@ -1,2 +0,0 @@
-// Re-export everything from the shared supabase package
-export * from '@workspace/supabase/supabase' 
--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -1,12 +1,18 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
     port: 3001,
   },
-  plugins: [tsConfigPaths(), tanstackStart()],
-
-})
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart({ customViteReactPlugin: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
+});
